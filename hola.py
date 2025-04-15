6d2c4f68fdef703803bd317e946ae268e0b738f7
nkls-1 <nikolas.navia.godoy@gmail.com>
2025-04-15 10:21:02 -0400

    ejemplos de variables

--- hola.py
+++ hola.py
@@ -1,1 +1,18 @@
-print("Hola Mundo")
+# variables
+# nombre="Niko"
+
+
+
+# ejemplos de concatenacion
+# print("Hola",nombre,", y su edad es",edad)
+
+# print("Hola",nombre,", y su edad en 5 años será",edad+5)
+
+# num1=45
+# num2=9
+# print(num1+num2)
+
+# print ("ingrese 1 numero")
+n1=int(input(" ingrese 1 numero"))
+n2=int(input(" ingrese 1 numero"))
+print("el resultado de la suma es ", n1+n2)
